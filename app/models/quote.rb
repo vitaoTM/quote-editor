613903059d1e49193f7d267344b4e0fad5534ed4
vitor <vitor.grosskopf@gmail.com>
2025-05-05 17:42:51 -0300

    Undertood both ways to set broadcast callbacks
    - Turbo frames and stream, broadcast creation, updates and deletion

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,9 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" } # , partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+
+  broadcast_to ->(quote) { "quotes" }, insert_by: :prepend
 end
