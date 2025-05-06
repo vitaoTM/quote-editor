59dd15bb56cf95f993e5658d6b7562e1d0699d44
vitor <vitor.grosskopf@gmail.com>
2025-05-06 16:41:34 -0300

    Create Pages controller
    - Add home action to controller
    - update routes root to #home action in pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "quotes#index"
+  root to: "pages#home"
 end
