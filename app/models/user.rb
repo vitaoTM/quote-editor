6b6e370e5487a4e71819bc3fa8b02584ed9bb481
vitor <vitor.grosskopf@gmail.com>
2025-05-06 16:44:40 -0300

    Create method: name for user model to be available on views
    - Create helper method for company, to be used in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :validatable
   belongs_to :company
+
+  def name
+    email.split("@").first.capitalize
+  end
 end
