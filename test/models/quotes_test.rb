ab813daf2d567144b39f6a7cf0c4c4bc55a91621
vitor <vitor.grosskopf@gmail.com>
2025-05-07 16:01:04 -0300

    Comment tests

--- test/models/quotes_test.rb
+++ test/models/quotes_test.rb
@@ -1,50 +1,50 @@
 require "test_helper"
 
-class QuotesTest < ActiveSupport::TestCase
-  setup do
-    loggin_as users(:accountant)
-    @quote = Quote.ordered.first # quotes(:first)
-  end
-  test "showing a quote" do
-    visit quotes_path
-    click_link @quote.name
+# class QuotesTest < ActiveSupport::TestCase
+#   setup do
+#     loggin_as users(:accountant)
+#     @quote = Quote.ordered.first # quotes(:first)
+#   end
+#   test "showing a quote" do
+#     visit quotes_path
+#     click_link @quote.name
 
-    assert_selector "h1", text: @quote.name
-  end
+#     assert_selector "h1", text: @quote.name
+#   end
 
-  test "Creating a new quote" do
-    visit quotes_path
-    assert_selector "h1", text: "Quotes"
+#   test "Creating a new quote" do
+#     visit quotes_path
+#     assert_selector "h1", text: "Quotes"
 
-    click_on "New quote"
-    fill_in "Name", with: "Capybara quote"
+#     click_on "New quote"
+#     fill_in "Name", with: "Capybara quote"
 
-    assert_selector "h1", text: "Quotes"
-    click_on "Create quote"
+#     assert_selector "h1", text: "Quotes"
+#     click_on "Create quote"
 
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
-  end
+#     assert_selector "h1", text: "Quotes"
+#     assert_text "Capybara quote"
+#   end
 
-  test "Updating a quote" do
-    visit quotes_path
-    assert_selector "h1", text: "Quotes"
+#   test "Updating a quote" do
+#     visit quotes_path
+#     assert_selector "h1", text: "Quotes"
 
-    click_on "Edit", match: :first
-    fill_in "Name", with: "Updated quote"
+#     click_on "Edit", match: :first
+#     fill_in "Name", with: "Updated quote"
 
-    assert_selector "h1", text: "Quotes"
-    click_on "Update quote"
+#     assert_selector "h1", text: "Quotes"
+#     click_on "Update quote"
 
-    assert_selector "h1", text: "Quotes"
-    assert_text "Updated quote"
-  end
+#     assert_selector "h1", text: "Quotes"
+#     assert_text "Updated quote"
+#   end
 
-  test "Destroying a quote" do
-    visit quotes_path
-    assert_text @quote.name
+#   test "Destroying a quote" do
+#     visit quotes_path
+#     assert_text @quote.name
 
-    click_on "Delete", match: :first
-    assert_no_text @quote.name
-  end
-end
+#     click_on "Delete", match: :first
+#     assert_no_text @quote.name
+#   end
+# end
