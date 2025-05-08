04c1401a21b589e2f6121525269d25292347351d
vitor <vitor.grosskopf@gmail.com>
2025-05-08 19:48:53 -0300

    Add associtations Quote - LineItemDate
    - Update routes for line item dates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [ :index, :show ]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
