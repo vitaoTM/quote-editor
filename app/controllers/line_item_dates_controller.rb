3f9917b0815e8b0e52b8e18fd4cd8444c1e2a49d
vitor <vitor.grosskopf@gmail.com>
2025-05-09 23:50:18 -0300

    Adding turbo stream to view

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -10,7 +10,10 @@ class LineItemDatesController < ApplicationController
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
 
     if @line_item_date.save
-      redirect_to quote_path(@quote), notice: "Date was created"
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was created" }
+        format.turbo_stream { flash.now[:notice] = "Data was created" }
+      end
     else
       render :new, status: :unprocessable_entity
     end
