789593d69774b7b01d70b34c6e0e6cb8f23fd909
vitor <vitor.grosskopf@gmail.com>
2025-05-10 00:14:26 -0300

    Fix typo

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -5,6 +5,6 @@ class LineItemDate < ApplicationRecord
   scope :ordered, -> { order(date: :asc) }
 
   def previous_date
-    quote.line_item_dates.oredered.where("date < ?", date).last
+    quote.line_item_dates.ordered.where("date < ?", date).last
   end
 end
