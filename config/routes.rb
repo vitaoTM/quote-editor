46ff2fb7b3da80b067687d922274dccdb9e60a6c
vitor <vitor.grosskopf@gmail.com>
2025-05-12 15:41:46 -0300

    Update line_items routes
    - Fix typo
    - Add line_item fixtures
    - Run: rails db:fixtures:load

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :quotes do
-    resources :line_item_dates, except: [ :index, :show ]
+    resources :line_item_dates, except: [ :index, :show ] do
+      resources :line_items, except: [ :index, :show ]
+    end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
