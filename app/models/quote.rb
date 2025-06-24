e8fc6ea9a990875a1761f4b2d96730f6069633cd
vitor <vitor.grosskopf@gmail.com>
2025-06-24 15:42:17 -0300

    designing navbar

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,7 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   scope :ordered, -> { order(id: :desc) }
   # after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
@@ -9,4 +10,8 @@ class Quote < ApplicationRecord
   # after_destroy_commit -> { broadcast_remove_to "quotes" } # , partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 
   broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, insert_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
